3b8c4617b8f4303048d0414731e487f87f6f7b21
mathias <mathias04@orange.fr>
2021-05-15 22:45:39 +0200

    Add Logic into ViewModel

--- app/src/main/java/com/example/myapplication/presentation/list/PokemonListFragment.kt
+++ app/src/main/java/com/example/myapplication/presentation/list/PokemonListFragment.kt
@@ -6,7 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.core.os.bundleOf
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
@@ -23,6 +26,8 @@ class PokemonListFragment : Fragment() {
 
 
     private lateinit var recyclerview: RecyclerView
+    private lateinit var loader: ProgressBar
+    private lateinit var textViewError: TextView
 
     private val adapter = PokemonAdapter(listOf(), ::onClickedPokemon)
 
@@ -40,13 +45,21 @@ class PokemonListFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         recyclerview = view.findViewById(R.id.pokemon_recyclerview)
+        loader = view.findViewById(R.id.pokemon_loader)
+        textViewError = view.findViewById(R.id.pokemon_error)
 
         recyclerview.apply {
             layoutManager = LinearLayoutManager(context)
             adapter = this@PokemonListFragment.adapter
         }
-        viewModel.pokeList.observe(viewLifecycleOwner, Observer { list  ->
-            adapter.updateList(list)
+        viewModel.pokeList.observe(viewLifecycleOwner, Observer { pokemonModel  ->
+            loader.isVisible = pokemonModel is PokemonLoader
+            textViewError.isVisible = pokemonModel is PokemonError
+
+
+                if(pokemonModel is PokemonSuccess) {
+                    adapter.updateList(pokemonModel.pokeList)
+                }
 
         })
 
